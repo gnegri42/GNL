ec6aa9e7bd1e8a86db78b0e6409011566ce78db0
Geoffrey NEGRI <gnegri@e3r10p9.42.fr>
2017-11-24 16:05:34 +0100

    Modified GNL.c. Working but to be improve

--- get_next_line.c
+++ get_next_line.c
@@ -10,22 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		get_next_line(const int fd, char **line)
-{
-	int			ret;
-	char		buf[BUF_SIZE];
-	char		*dup;
-	static int	line_nbr;
-	int			i;
+#include "libft/libft.h"
+#include "get_next_line.h"
 
-	i = 0;
-	while ((ret = read(fd, buf, BUF_SIZE)) > 0)
+int	get_next_line(const int fd, char **line)
+{
+	static char	*str;
+	int			i;
+	char		buff[BUFF_SIZE + 1];
+	int			ret;
+
+	if (!str && (str = (char *)ft_memalloc(sizeof(char))) == NULL)
+		return (-1);
+	while ((ret = read(fd, buff, BUFF_SIZE)) != 0)
 	{
-		if ((dup = ft_strdup(buf)) == NULL)
-			return (NULL);
-		line = ft_strsplit(buf, '\n');
-		ft_putstr(line[line_nbr]);
-		line_nbr++;
+		if (ret < 0 || fd < 0 || line == NULL)
+			return (-1);
+		buff[ret] = '\0';
+		str = ft_strjoin(str, buff);
+		if (!str)
+			return (-1);
 	}
-	
+	i = 0;
+	if (str[i])
+	{
+		while (str[i] != '\n' && str[i])
+			i++;
+		*line = ft_strsub(str, 0, i);
+		str = &str[i + 1];
+		return (1);
+	}
+	if (!*line)
+		return (-1);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,8 +13,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
 # define BUFF_SIZE 32
 
-int		get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line);
 
 #endif
