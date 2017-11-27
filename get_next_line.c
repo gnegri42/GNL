f16ba37783d3e5410b0572c279327e12e1f09623
Geoffrey NEGRI <gnegri@e3r10p11.42.fr>
2017-11-27 16:19:32 +0100

    Splitted in 3 fonctions and added multi fd compatibility

--- get_next_line.c
+++ get_next_line.c
@@ -13,34 +13,72 @@
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+static int			ft_is_line(char **str, char **line)
 {
-	static char	*str;
+	char		*tmp;
+	char		*str_l;
 	int			i;
-	char		buff[BUFF_SIZE + 1];
-	int			ret;
 
-	if (!str && (str = (char *)ft_memalloc(sizeof(char))) == NULL)
-		return (-1);
-	while ((ret = read(fd, buff, BUFF_SIZE)) != 0)
-	{
-		if (ret < 0 || fd < 0 || line == NULL)
-			return (-1);
-		buff[ret] = '\0';
-		str = ft_strjoin(str, buff);
-		if (!str)
-			return (-1);
-	}
 	i = 0;
-	if (str[i])
+	str_l = *str;
+	while (str_l[i] != '\n')
 	{
-		while (str[i] != '\n' && str[i])
-			i++;
-		*line = ft_strsub(str, 0, i);
-		str = &str[i + 1];
-		return (1);
+		if (str_l[i] == '\0')
+			return (0);
+		i++;
 	}
-	if (!*line)
-		return (-1);
+	tmp = &str_l[i];
+	*tmp = '\0';
+	*line = ft_strdup(*str);
+	*str = ft_strdup(tmp + 1);
+	free(str_l);
+	return (1);
+}
+
+static int			ft_read_data(int fd, char *buff, char **str, char **line)
+{
+	int			ret;
+	char		*tmp;
+
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		if (*str)
+		{
+			tmp = *str;
+			*str = ft_strjoin(tmp, buff);
+			free(tmp);
+			tmp = NULL;
+		}
+		else
+			*str = ft_strdup(buff);
+		if (ft_is_line(str, line) == 1)
+			return (1);
+	}
 	return (0);
+}
+
+int					get_next_line(int const fd, char **line)
+{
+	static char	*str[2147483647];
+	char		*buff;
+	int			ret;
+	int			i;
+
+	if (line == NULL || fd < 0 ||
+			fd > 2147483647 || (read(fd, str[fd], 0) < 0)
+			|| !(buff = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
+		return (-1);
+	if (str[fd] && ft_is_line(&str[fd], line))
+		return (1);
+	i = 0;
+	ft_bzero(buff, BUFF_SIZE + 1);
+	ret = ft_read_data(fd, buff, &str[fd], line);
+	free(buff);
+	if (ret != 0 || str[fd] == NULL || str[fd][0] == '\0')
+		return (ret);
+	*line = str[fd];
+	str[fd] = NULL;
+	free(str[fd]);
+	return (1);
 }
